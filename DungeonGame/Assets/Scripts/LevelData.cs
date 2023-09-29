158af8c687d31c4858d415a8735c5f310c4fd994
Khairullin Ruslan <ruslanrbb8@gmail.com>
2023-09-29 15:28:11 +0300

    add ScriptableObjects for Levels

---
+++ DungeonGame/Assets/Scripts/LevelData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Level
+{
+    [CreateAssetMenu(fileName = "New LevelData", menuName = "LevelData")]
+    public class LevelData : ScriptableObject
+    {
+        [SerializeField]
+        private int _numberOfLevel;
+        [SerializeField]
+        private Object[] _enemies; //я не знаю как мы потом реализуем наполнение врагами,
+                                   //через Enum или GameObject или вообще через Dictionary, так что оставлю пока просто Object
+        public int NumberOfLevel => _numberOfLevel;
+        public Object[] Enemies => _enemies;
+    }
+}
