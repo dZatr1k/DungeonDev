10f215e20b5eda958228b5a6124ff93808f18b25
dZatr1k <uhuman239@gmail.com>
2023-11-01 00:08:54 +0300

    small fixes and additions

---
+++ DungeonGame/Assets/Scripts/Field/CellSpriteRandomizer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellSpriteRandomizer : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _sprites;
+
+    private void Start()
+    {
+        int randomSpriteIndex = Random.Range(0, _sprites.Count);
+        var renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = _sprites[randomSpriteIndex];
+
+        bool isFlipedX = Random.Range(0, 2) == 1;
+        renderer.flipX = isFlipedX;
+
+        enabled = false;
+    }
+}
